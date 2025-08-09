3888fe08da5f3ce029aa8b2d0b8376f1e010e5eb
azbrica <azbrica1603@gmail.com>
2025-08-09 09:51:03 +0700

    Update script.js

--- game/hadiah1/script.js
+++ game/hadiah1/script.js
@@ -1,5 +1,17 @@
-const lyrics = ["Your morning eyes, I could stare like watching stars", "I could walk you by, and I'll tell without a thought", "You'd be mine", "Would you mind if I took your hand tonight", "Know you're all that I want this life", "I'll imagine we fell in love", "I'll nap under moonlight skies with you", "I think I'll picture us, you with the waves", "The oceans colors on your face", "I'll leave my heart with your air", "So let me fly with you", "Will you be forever with me", "[Guitar]", "My love will always stay by you", "I'll keep it safe so don't you worry a thing,", "I'll tell you I love you more", "It's stuck with you forever so promise you won't let it go", "I'll trust the universe will always bring me to you", "I'll imagine we fell in love", "I'll nap under moonlight skies with you", "I think I'll picture us, you with the waves", "The oceans colors on your face", "I'll leave my heart with your air", "So let me fly with you", "Will you be forever with me"];
-const delays = [400, 300, 300, 400, 3300, 200, 200, 200, 400, 400, 400, 400, 30000, 400, 400, 400, 400, 400, 400, 400, 400, 400, 400, 1000];
+const lyrics = [
+  "Living all alone kinda forgot it's been that long",
+  "Since someone's gone, I've been trying to be a little bit strong",
+  "And it is not that easy to be exactly who I was",
+  "My shit is done, now it's time for me try to moving on",
+  "'Cuz if you think I'm such a happy person, no you are wrong",
+  "By saying my laughter is louder than yours",
+  "Shut your freakin' mouth",
+  "No one knows what I feel and what I suffer, no they don't know",
+  "So keep your thoughts and stop assuming that",
+  "Someone is always fine"
+];
+
+const delays = [400, 400, 400, 400, 400, 400, 400, 400, 400, 1000];
 let currentIndex = 0;
 let currentCharIndex = 0;
 let currentLine = "";
